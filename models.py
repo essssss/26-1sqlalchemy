97ef6689b6a64f8e68f3d45360866e530e92f020
Steven <srmcnamara7@gmail.com>
2023-06-14 11:25:07 -0400

    added test files

--- models.py
+++ models.py
@@ -4,47 +4,43 @@ from sqlalchemy.orm import mapped_column
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
-	db.app = app
-	db.init_app(app)
+    db.app = app
+    db.init_app(app)
 
 
 # Define all our MODELS
-class Pet (db.Model):
-	"""A Pet"""
+class Pet(db.Model):
+    """A Pet"""
 
-	__tablename__ = "pets"
+    __tablename__ = "pets"
 
-	@classmethod
-	def get_by_species(cls, species):
-		return cls.query.filter_by(species=species).all()
-	
-	@classmethod
-	def get_all_hungry(cls):
-		"""return all pets with hunger > 20"""
-		return cls.query.filter(Pet.hunger > 20).all()
+    @classmethod
+    def get_by_species(cls, species):
+        return cls.query.filter_by(species=species).all()
 
-	def __repr__(self):
-		p = self
-		return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>"
+    @classmethod
+    def get_all_hungry(cls):
+        """return all pets with hunger > 20"""
+        return cls.query.filter(Pet.hunger > 20).all()
 
-	id = db.Column(db.Integer,
-				   primary_key=True,
-				   autoincrement=True)
+    def __repr__(self):
+        p = self
+        return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>"
 
-	name = db.Column(db.String(50),
-					 nullable=False,
-					 unique=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-	species = db.Column(db.String(30), nullable=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
+    species = db.Column(db.String(30), nullable=True)
 
-	hunger = db.Column(db.Integer, nullable=False, default=20)
+    hunger = db.Column(db.Integer, nullable=False, default=20)
 
-	def greet(self):
-		return f"Hi, I am {self.name} the {self.species}."
-	
-	def feed(self, amt=20):
-		"""Update hunger based off of amt"""
-		self.hunger -= amt
-		self.hunger = max(self.hunger, 0)
+    def greet(self):
+        return f"I'm {self.name} the {self.species}"
+
+    def feed(self, amt=20):
+        """Update hunger based off of amt"""
+        self.hunger -= amt
+        self.hunger = max(self.hunger, 0)
